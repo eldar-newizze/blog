f6c3b7eebcbbd2f62738bff5493ae2f8b373f48b
Zeref95 <niderman@stud.rksi.ru>
2020-11-17 20:58:00 +0300

    transfer completed

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::view('/', 'index')->name('index');
+Route::view('register', 'register')->name('register');
+Route::view('login', 'login')->name('login');
+Route::view('about', 'about')->name('about');
+Route::view('contact', 'contact')->name('contact');
+Route::view('single', 'single')->name('single');
+
+Route::view('blog1', 'blog1')->name('blog1');
+Route::view('blog2', 'blog2')->name('blog2');
+Route::view('blog3', 'blog3')->name('blog3');
